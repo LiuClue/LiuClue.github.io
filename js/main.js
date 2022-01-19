4397a282e668db28ce5f6bbfb20d02502fad7053
LiuClue <dennisliu323@gmail.com>
2022-01-19 18:25:26 -0500

    add caption

--- js/main.js
+++ js/main.js
@@ -129,6 +129,28 @@ $(document).ready(function () {
             type: 'image',
             removalDelay: 300,
             mainClass: 'mfp-with-zoom',
+            image: {
+                markup: '<div class="mfp-figure">' +
+                    '<div class="mfp-close"></div>' +
+                    '<div class="mfp-img"></div>' + // Floated left
+                    '<div class="mfp-title"></div>' + // This is floated right shows up on the right side
+                    '<div class="mfp-bottom-bar">' +
+                    '<div class="mfp-counter"></div>' +
+                    '</div>' +
+                    '</div>', // Popup HTML markup. `.mfp-img` div will be replaced with img tag, `.mfp-close` by close button
+
+
+                cursor: 'mfp-zoom-out-cur', // Class that adds zoom cursor, will be added to body. Set to null to disable zoom out cursor.
+
+                titleSrc: 'title', // Attribute of the target element that contains caption for the slide.
+                // Or the function that should return the title. For example:
+                // titleSrc: function(item) {
+                //   return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+                // }
+
+                verticalFit: true, // Fits image in area vertically
+
+            },
             gallery: {
                 enabled: true
             },
